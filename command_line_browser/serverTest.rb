037a5dbb6eab4f0a811400e46d2add16a4ffd8b4
Michael Le <michael-le14@hotmail.com>
2016-08-07 05:03:13 -0400

    finished post function and project

--- command_line_browser/serverTest.rb
+++ command_line_browser/serverTest.rb
@@ -1,4 +1,5 @@
-require 'socket'               # Get sockets from stdlib
+require 'socket'
+require 'json'
 
 server = TCPServer.new(2000)
 
@@ -17,14 +18,30 @@ loop do
   if File.exist?(path)
     if method == "GET"
       response_body = (File.open(path, 'r')).read
+      response_head = "HTTP/1.0 200 OK\n#{Time.now.ctime}\nContent-Length: #{response_body.length}\r\n\r\n"    
+    elsif method == "POST"
+      body = ""
+      while line = client.gets
+        body << line
+        break if body =~ /\r\n\r\n$/
+      end
+      body = body.chomp
+
+      params = JSON.parse(body)
+      list_entries = ""
+      params["viking"].each do |key, value|
+        list_entries += "<li>#{key}: #{value}</li>\n"
+      end
+
+      file = File.open(path)
+      response_body = file.read.gsub("<%= yield %>", list_entries)
       response_head = "HTTP/1.0 200 OK\n#{Time.now.ctime}\nContent-Length: #{response_body.length}\r\n\r\n"
     end
   else
     response_head = ("HTTP/1.0 404 Not Found\r\n\r\n")
     response_body = ("Sorry, file not found.")
   end
-
   client.puts(response_head)
   client.puts(response_body)
-  client.close                 # Disconnect from the client
+  client.close                
 end
--- command_line_browser/tinyWebBrowser.rb
+++ command_line_browser/tinyWebBrowser.rb
@@ -1,8 +1,37 @@
 require 'socket'
+require 'json'
 
 host = "localhost"
 port = 2000
-request = "GET /index.html HTTP/1.0\r\n\r\n"
+
+puts "Welcome to the tinyWebBrowser!"
+printf "What would you like to do POST or GET? "
+input = gets.upcase.chomp
+until input == "GET" or input == "POST"
+  printf "Would you like to POST or GET?"
+  input = gets.upcase.chomp
+end
+
+case input
+when "GET" then
+  request = "GET /index.html HTTP/1.0\r\n\r\n"
+when "POST" then
+  printf "Enter a name: "
+  name = gets.chomp
+  printf "Enter an email: "
+  email = gets.chomp
+
+  hash = {viking: {name: name, email=>email}}
+  hash_json = hash.to_json
+
+  request = %{
+POST /thanks.html HTTP/1.0
+From: #{email}
+Content-Type: application/json
+Content-Length: #{hash_json.length}
+\r\n\r\n#{hash_json}\r\n\r\n
+  }
+end
 
 socket = TCPSocket.open(host,port)
 socket.print(request)
