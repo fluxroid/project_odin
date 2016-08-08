2cec09ca5ea42b76f95f763807f074849c3622f1
Michael Le <michael-le14@hotmail.com>
2016-08-08 01:41:17 -0400

    Added front end view

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,3 +1,13 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	message = params['message'].to_s
+	shift = params['shift'].to_i
+	encoded_message = caesar_cipher(message, shift)
+	erb :index, :locals => {:encoded_message => encoded_message} 
+end
+
 def caesar_cipher(text, shift)
 	text.chars.map do |c|
 		if c.match(/[a-zA-Z]/)
