f6d8c41dbd6d39298bead92982c4dc69f7581a4e
Michael Le <michael-le14@hotmail.com>
2017-01-28 22:41:21 -0500

    Finish user signup ch7

---
+++ caesar_cipher/config.ru
@@ -0,0 +1,2 @@
+require './caesar_cipher'
+run Sinatra::Application
--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get '/about', to: 'staticpages#about'
   get '/contact', to: 'staticpages#contact'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
